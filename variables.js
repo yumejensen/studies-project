d5985ec92caaf6a411dcf261f839280db1edbeb6
yumejensen <yumejensen@gmail.com>
2025-04-10 05:47:24 +0000

    variables study

--- variables.js
+++ variables.js
@@ -1,6 +1,25 @@
 /**
  * VARIABLES:
  * 
- * 0: (provide general description of the concept)
+ * 0: Intro
+ * Variables are names which point to data in JavaScript. They can also be thought of as "containers" that hold data. 
+ * Variables can hold many data types, such as strings, numbers, objects, arrays, and functions. 
+ * The variable naming convention in JavaScript is called camelCase, where the first word is lowercase 
+ * and each following word has an uppercased first letter.
+ * 
+ * 
+ * 1: Declaration and Assignment
+ * Variables are declared with the keywords var, let, and const.
+ * Simply declaring a variable would be a keyword followed by the variable name.
+ * Assigning a variable to a value uses the assignment operator =.
+ * var myName;
+ * myName = "Yume";
+ * 
+ * Variables can be declared and assigned in the same line. This is called initialization.
+ * var myAge = 25;
+ * let classData = {name: "Alex Aaron", occupation: "Instructor"}
+ * const addTen = (num) => num + 10;
+ * 
+ * There is also a method of declaring variables called "destructuring"
  * 
  */
