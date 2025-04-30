af90d4b2ba17f1b1f6905112d53bdc6be3ee9a90
yumejensen <yumejensen@gmail.com>
2025-04-30 13:29:43 -0500

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -15,62 +15,87 @@
  * // NUMBER //
  * Numbers are numeric data, stored as a decimal (floating point). Numbers can be written with or without decimals.
  * Numbers can also be written with exponential notation.
- * 
- * let x = 25;
- * let y = 25.00;
- * let z = 25e5; // 2500000
- * 
+ */
+    let x = 25;
+    let y = 25.00;
+    let z = 25e5; // 2500000
+
+/** 
  * // BIGINT //
  * BigInt variables are used to store integer values that are too big to be represented by a normal number. 
  * Normal numbers lose accuracy after 15 digits.
  * BigInt are created by declaring with BigInt() or adding n to the end of an integer.
- * 
- * let b = 100000000000000000n;
- * let c = BigInt(100000000000000000);
- * 
+ */
+    let b = 100000000000000000n;
+    let c = BigInt(100000000000000000);
+
+/** 
  * // STRING //
  * Strings are character data and are written with single or double quotes on the outside. Quotes can exist inside
  * a string if they are different. To avoid this problem altogether, the backslash can be used to escape certain characters.
  * The backslash is also used to escape apostraphes and backslashes.
- * 
- * let string1 = "He is called 'Sam' not 'Sammy'";
- * let string2 = "He is called \"Sam\" not \"Sammy\""; // prints  "He is called 'Sam' not 'Sammy'"
- * 
+ */
+    let string1 = "He is called 'Sam' not 'Sammy'";
+    let string2 = "He is called \"Sam\" not \"Sammy\""; // prints  "He is called 'Sam' not 'Sammy'"
+
+/** 
  * // BOOLEAN // 
  * Booleans are true and false. They are used in conditional testing, such as if statements. 
  * Most of the time, booleans do not need to be declared explicitly because JavaScript converts values 
  * to booleans automatically in a boolean context by evaluating truthy and falsy.
  * All values are inherently truthy except: false, 0, -0, "", null, undefined, NaN
- * 
- * if ("hello") { // resolves to true
- *   console.log("this is truthy");
- * }
- * // "hello" is truthy, therefore the code prints "this is truthy"
- * 
+ */
+    if ("hello") { // resolves to true
+    console.log("this is truthy");
+    };
+    // "hello" is truthy, therefore the code prints "this is truthy"
+
+/** 
  * // UNDEFINED //
  * No value, not initialized
- * 
- * // NULL //
+ */
+
+
+/** // NULL //
  * No value, intentionally nullified by a programmer
- * 
- * // NaN //
+ */
+
+
+/** // NaN //
  * Not a number
- * 
- * --------------------------------------------------------------------------------------------------------------------------
+ */
+
+
+/** --------------------------------------------------------------------------------------------------------------------------
  * 2: COMPLEX DATA TYPES
  * 
- * // ARRAY
- * // OBJECT
- * // FUNCTION
+ * // ARRAY //
  * 
- * --------------------------------------------------------------------------------------------------------------------------
- * 10: INFINITY AND -INFINITY
  * 
- * --------------------------------------------------------------------------------------------------------------------------
- * 11: SIMPLE VS COMPLEX DATA TYPES
+ 
+/**
+ * // OBJECT //
  * 
- * --------------------------------------------------------------------------------------------------------------------------
- * 12: COPY BY VALUE VS COPY BY REFERENCE 
+ */
+
+/**
+ * // FUNCTION //
  * 
+ */
+
+/** --------------------------------------------------------------------------------------------------------------------------
+ * 3: INFINITY AND -INFINITY
+ * 
+ */
+
+
+/** --------------------------------------------------------------------------------------------------------------------------
+ * 4: SIMPLE VS COMPLEC DATA TYPES
+ * 
+ */
+
+
+/** --------------------------------------------------------------------------------------------------------------------------
+ * 5: COPY BY VALUE VS COPY BY REFERENCE
  * 
  */
