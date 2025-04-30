c000020f42092e2a8d5a9d3a5f13fbbc94bf1ce7
yumejensen <yumejensen@gmail.com>
2025-04-30 12:18:54 -0500

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -3,8 +3,14 @@
  * --------------------------------------------------------------------------------------------------------------------------
  * 0: INTRO
  * 
+ * Data is categorized into 8 different types in JavaScript. Knowing datatypes is crucial because the 
+ * JavaScript interpreter will handle each data type differently. 
+ * The two big categories are simple/primitive datatypes and complex datatypes.
+ * Simple dataypes are atomic, immutable, and copy by value. 
+ * Complex datatypes are mutable, an indefinite size, and copy by reference.
+ * 
  * --------------------------------------------------------------------------------------------------------------------------
- * 1: SIMPLE DATA TYPES:
+ * 1: SIMPLE/ PRIMITIVE DATA TYPES:
  * // NUMBER
  * // STRING
  * // BOOLEAN
