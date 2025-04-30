13c214f8e2c536c96d1a1ded34fe955fb568cc6d
yumejensen <yumejensen@gmail.com>
2025-04-30 12:42:40 -0500

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -10,16 +10,40 @@
  * Complex datatypes are mutable, an indefinite size, and copy by reference.
  * 
  * --------------------------------------------------------------------------------------------------------------------------
- * 1: SIMPLE/ PRIMITIVE DATA TYPES:
- * // NUMBER
- * // STRING
- * // BOOLEAN
- * // UNDEFINED
- * // NULL
- * // NaN
+ * 1: SIMPLE/ PRIMITIVE DATA TYPES
+ * 
+ * // NUMBER //
+ * Numbers are numeric data, stored as a decimal (floating point). Numbers can be written with or without decimals.
+ * Numbers can also be written with exponential notation.
+ * let x = 25;
+ * let y = 25.00;
+ * let z = 25e5; // 2500000
+ * 
+ * // BIGINT //
+ * BigInt variables are used to store integer values that are too big to be represented by a normal number. 
+ * Normal numbers lose accuracy after 15 digits.
+ * BigInt are created by declaring with BigInt() or adding n to the end of an integer.
+ * let b = 100000000000000000n;
+ * let c = BigInt(100000000000000000);
+ * 
+ * // STRING //
+ * Strings are character data and are written with single or double quotes on the outside.
+ * 
+ * // BOOLEAN // 
+ * true or false
+ * 
+ * // UNDEFINED //
+ * No value, not initialized
+ * 
+ * // NULL //
+ * No value, intentionally nullified by a programmer
+ * 
+ * // NaN //
+ * Not a number
  * 
  * --------------------------------------------------------------------------------------------------------------------------
  * 2: COMPLEX DATA TYPES
+ * 
  * // ARRAY
  * // OBJECT
  * // FUNCTION
@@ -33,5 +57,5 @@
  * --------------------------------------------------------------------------------------------------------------------------
  * 12: COPY BY VALUE VS COPY BY REFERENCE 
  * 
- *
+ * 
  */
