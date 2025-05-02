f82ed45b35b354b7e6be20324b311f9ab0522c71
yumejensen <yumejensen@gmail.com>
2025-05-02 17:40:22 -0500

    loops

--- loops.js
+++ loops.js
@@ -2,11 +2,35 @@
  * LOOPS:
  * --------------------------------------------------------------------------------------------------------------------------
  * 0: INTRO
- * 
+ * Loops are an easy way to repeat actions a certain number of times. For example, instead of needing to write 5 lines of 
+ * code to perform an action 5 times, we can write one loop that repeats itself 5 times. 
+ * Although the core purpose is the same, each kind of loop has a different strength or purpose. For example,
+ * a for loop can iterate over an array or a string, and a for-in loop can iterate over an object.
  * 
  * --------------------------------------------------------------------------------------------------------------------------
  * 1: WHILE, FOR, AND FOR-IN LOOPS
  * 
+ * A while loop has the following syntax:
+ *  while (condition) {
+ *    statement
+ *  }
+ * The loop will continue as long as the condition is true and stop executing when the condition becomes false.
+ */
+
+/**
+ * A for loop has the following syntax:
+ *  for (initialization; condition; update) {
+ *     statement
+ *  }
+ * The loop will initialize a counter, have a condition, and an update expression. Once the condition becomes false, 
+ * the loop will end.
+ */
+
+/**
+ * A for-in loop has the following syntax:
+ *  for (variable in object){
+ *     statement 
+ *  }
  */
 
 /** --------------------------------------------------------------------------------------------------------------------------
